6954e70092b32e676de8819dcc92a72d6def0a3b
Alexey Taktarov <molefrog@gmail.com>
2022-10-30 18:27:49 +0100

    ConnectTheDots, a standalone attraction.

---
+++ src/attractions/ConnectTheDots/index.tsx
@@ -0,0 +1,171 @@
+import styled from "@emotion/styled";
+import { useRef, useState, useCallback } from "react";
+
+type Coords = [number, number];
+
+interface Props {
+  dots: Coords[];
+  baseWidth?: number;
+  image?: string;
+}
+
+export const ConnectTheDots = ({ dots, image, baseWidth = 670 }: Props) => {
+  const [pointer, setPointer] = useState<Coords>();
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [connIndices, setConnIndices] = useState<number[]>([]);
+
+  const svgRef = useRef<SVGSVGElement>(null);
+
+  // the path that is currently being built
+  const wipPath = buildPath([...connIndices.map((i) => dots[i]), pointer]);
+
+  // to draw the final path
+  const finalPath = buildPath(connIndices.map((i) => dots[i]));
+
+  const reset = useCallback(() => {
+    setConnIndices([]);
+    setIsDrawing(false);
+    setPointer(undefined);
+  }, []);
+
+  const handleDotClick = useCallback(
+    (idx: number, e: React.MouseEvent) => {
+      e.stopPropagation();
+
+      // don't allow two sequential points
+      if (connIndices[connIndices.length - 1] === idx) return reset();
+
+      setConnIndices((arr) => [...arr, idx]);
+      setIsDrawing(true);
+    },
+    [connIndices]
+  );
+
+  // Update pointer position
+  const handleMouseMove = useCallback(
+    (e: React.MouseEvent) => {
+      if (!isDrawing || !svgRef.current) return;
+      setPointer(getSVGCoords(e, svgRef.current));
+    },
+    [isDrawing]
+  );
+
+  // Stop drawing when gets out of bounds
+  const handleClick = useCallback(() => reset(), []);
+  const handleMouseLeave = useCallback(() => reset(), []);
+
+  return (
+    <Figure>
+      {image && <img src={image} />}
+
+      <SVG
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox={`0 0 ${baseWidth} ${baseWidth}`}
+        ref={svgRef}
+        onClick={handleClick}
+        onMouseMove={handleMouseMove}
+        onMouseLeave={handleMouseLeave}
+      >
+        {wipPath && (
+          <path d={wipPath} fill="none" stroke="#ccc" strokeWidth={6} strokeDasharray="6, 1" />
+        )}
+
+        {pointer && <circle fill="purple" r={10} cx={pointer[0]} cy={pointer[1]} />}
+
+        {dots.map((pos, index) => (
+          <Dot key={pos.toString()} position={pos} onClick={handleDotClick.bind(this, index)} />
+        ))}
+      </SVG>
+    </Figure>
+  );
+};
+
+const norm = (a: Coords, b: Coords) => {
+  const [[ax, ay], [bx, by]] = [a, b];
+  const { sqrt, pow } = Math;
+
+  const len = sqrt(pow(bx - ax, 2) + pow(by - ay, 2));
+  return len > 0 ? [(bx - ax) / len, (by - ay) / len] : [0, 0];
+};
+
+/*
+ * Constructs an SVG path from the list of coordinates
+ */
+const buildPath = (dots: Array<Coords | undefined>): string => {
+  const filtered = dots.filter(Boolean) as Coords[];
+
+  return filtered
+    .map(([x, y], index) => {
+      if (index === 0) {
+        return `M ${x} ${y}`;
+      } else if (index == 1) {
+        const [px, py] = filtered[0];
+        const [nx, ny] = norm([px, py], [x, y]);
+
+        return `L ${x} ${y}`;
+        // return `Q ${px + nx} ${py + ny} ${x} ${y}`;
+      } else {
+        return `L ${x} ${y}`;
+        // return `T ${x} ${y}`;
+      }
+    })
+    .join(",");
+};
+
+interface DotProps extends React.ComponentProps<"circle"> {
+  position: Coords;
+}
+
+const Dot = ({ position: [x, y], ...rest }: DotProps) => {
+  return (
+    <>
+      <circle r={12} cx={x} cy={y} fill="white" />
+      <DotCircle r={7} cx={x} cy={y} {...rest}></DotCircle>
+    </>
+  );
+};
+
+const DotCircle = styled.circle`
+  fill: white;
+  stroke: black;
+  stroke-width: 2.5;
+
+  cursor: pointer;
+  &:hover {
+    fill: var(--color-selected-light);
+  }
+`;
+
+/**
+ * Translates page event coordinates to SVG coordinates
+ */
+const getSVGCoords = (event: React.MouseEvent, svg: SVGSVGElement): Coords => {
+  const pt = svg.createSVGPoint();
+
+  // pass event coordinates
+  pt.x = event.clientX;
+  pt.y = event.clientY;
+
+  // transform to SVG coordinates
+  const { x, y } = pt.matrixTransform(svg.getScreenCTM()?.inverse());
+  return [x, y];
+};
+
+/**
+ * Styles
+ */
+const Figure = styled.figure`
+  display: block;
+  margin: 0;
+  position: relative;
+
+  > img {
+    width: 100%;
+  }
+`;
+
+const SVG = styled.svg`
+  position: absolute;
+  inset: 0 0 0 0;
+`;
--- src/components/PlaygroundPage/index.tsx
+++ src/components/PlaygroundPage/index.tsx
@@ -1,13 +1,28 @@
 import styled from "@emotion/styled";
 import { useRef, useState, useCallback } from "react";
+
 import { TV } from "~/attractions/TV";
+import { ConnectTheDots } from "~/attractions/ConnectTheDots";
+import mapImage from "~/chapters/3-three/map.webp";
 
 export default function PlaygroundPage() {
   return (
     <Container>
       You can only <TV video="dQw4w9WgXcQ">see this</TV> in dev environment!
-      <br />
-      <Points />
+      <Block>
+        <ConnectTheDots
+          baseWidth={670}
+          dots={[
+            [187, 338],
+            [206, 498],
+            [338, 537],
+            [475, 500],
+            [562, 315],
+            [404, 312],
+          ]}
+          image={mapImage}
+        />
+      </Block>
     </Container>
   );
 }
@@ -18,146 +33,6 @@ const Container = styled.div`
   margin: 0 auto;
 `;
 
-/**
- * SVG Exp
- */
-type Coords = [number, number];
-
-const Points = () => {
-  const [pointer, setPointer] = useState<Coords>();
-  const [isDrawing, setIsDrawing] = useState(false);
-  const [connIndices, setConnIndices] = useState<number[]>([]);
-
-  const svgRef = useRef<SVGSVGElement>(null);
-
-  const dots: Coords[] = [
-    [120, 30],
-    [400, 320],
-    [40, 200],
-    [500, 400],
-  ];
-
-  // the path that is currently being built
-  const wipPath = buildPath([...connIndices.map((i) => dots[i]), pointer]);
-
-  // to draw the final path
-  const finalPath = buildPath(connIndices.map((i) => dots[i]));
-
-  const reset = useCallback(() => {
-    setConnIndices([]);
-    setIsDrawing(false);
-    setPointer(undefined);
-  }, []);
-
-  const handleDotClick = useCallback(
-    (idx: number, e: React.MouseEvent) => {
-      e.stopPropagation();
-
-      // don't allow two sequential points
-      if (connIndices[connIndices.length - 1] === idx) return reset();
-
-      setConnIndices((arr) => [...arr, idx]);
-      setIsDrawing(true);
-    },
-    [connIndices]
-  );
-
-  // Update pointer position
-  const handleMouseMove = useCallback(
-    (e: React.MouseEvent) => {
-      if (!isDrawing || !svgRef.current) return;
-      setPointer(getSVGCoords(e, svgRef.current));
-    },
-    [isDrawing]
-  );
-
-  // Stop drawing when gets out of bounds
-  const handleClick = useCallback(() => reset(), []);
-  const handleMouseLeave = useCallback(() => reset(), []);
-
-  return (
-    <>
-      <svg
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 670 670"
-        ref={svgRef}
-        onClick={handleClick}
-        onMouseMove={handleMouseMove}
-        onMouseLeave={handleMouseLeave}
-      >
-        {wipPath && (
-          <path d={wipPath} fill="none" stroke="#ccc" strokeWidth={6} strokeDasharray="6, 1" />
-        )}
-
-        {pointer && <circle fill="purple" r={10} cx={pointer[0]} cy={pointer[1]} />}
-
-        {dots.map((pos, index) => (
-          <Dot key={pos.toString()} position={pos} onClick={handleDotClick.bind(this, index)} />
-        ))}
-      </svg>
-    </>
-  );
-};
-
-/*
- * Constructs an SVG path from the list of coordinates
- */
-const buildPath = (dots: Array<Coords | undefined>): string => {
-  const filtered = dots.filter(Boolean) as Coords[];
-
-  return filtered
-    .map(([x, y], index) => {
-      if (index === 0) {
-        return `M ${x} ${y}`;
-      } else if (index == 1) {
-        const [px, py] = filtered[0];
-        const cx = x; // (px - x) * 0.5;
-        const cy = y; // -1 * (py - y) * 0.5;
-
-        return `Q ${x} ${y} ${cx} ${cy}`;
-      } else {
-        return `T ${x} ${y}`;
-      }
-    })
-    .join(",");
-};
-
-interface DotProps extends React.ComponentProps<"circle"> {
-  position: Coords;
-}
-
-const Dot = ({ position: [x, y], ...rest }: DotProps) => {
-  return (
-    <>
-      <circle r={12} cx={x} cy={y} fill="white" />
-      <DotCircle r={7} cx={x} cy={y} {...rest}></DotCircle>
-    </>
-  );
-};
-
-const DotCircle = styled.circle`
-  fill: white;
-  stroke: black;
-  stroke-width: 2.5;
-
-  cursor: pointer;
-  &:hover {
-    fill: var(--color-selected-light);
-  }
+const Block = styled.div`
+  margin: 24px 0;
 `;
-
-/**
- * Translates page event coordinates to SVG coordinates
- */
-const getSVGCoords = (event: React.MouseEvent, svg: SVGSVGElement): Coords => {
-  const pt = svg.createSVGPoint();
-
-  // pass event coordinates
-  pt.x = event.clientX;
-  pt.y = event.clientY;
-
-  // transform to SVG coordinates
-  const { x, y } = pt.matrixTransform(svg.getScreenCTM()?.inverse());
-  return [x, y];
-};
