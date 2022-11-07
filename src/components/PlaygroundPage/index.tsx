1415945b688dbe2b695306eaa28d2c903f510c47
Alexey Taktarov <molefrog@gmail.com>
2022-11-07 19:02:36 +0200

    Update playground.

--- src/components/PlaygroundPage/index.tsx
+++ src/components/PlaygroundPage/index.tsx
@@ -4,14 +4,26 @@ import { useRef, useState, useCallback } from "react";
 import { TV } from "~/attractions/TV";
 import { ConnectTheDots } from "~/attractions/ConnectTheDots";
 import { Emotion } from "~/attractions/Emotion";
+import { Spoiler } from "~/attractions/Spoiler";
 import mapImage from "~/chapters/3-three/map.webp";
 
 export default function PlaygroundPage() {
+  const [revealSpoiler, setRevealSpoiler] = useState(false);
+
   return (
     <Container>
       You can only <TV video="dQw4w9WgXcQ">see this</TV> in dev environment! There are whole variety
       of emotions a person can experience: <Emotion feeling="happiness">happiness</Emotion> and
       sadness
+      <p>
+        Что ж, всё оказалось не так плохо, как я думал.{" "}
+        <Spoiler reveal={revealSpoiler} onClick={() => setRevealSpoiler((t) => !t)}>
+          По крайней мере, я смог неторопливо проехать несколько метров. Я делал пару попыток, а
+          потом брал небольшой перерыв, и уже через час вождения окончательно перестроился.
+        </Spoiler>{" "}
+        Это было здорово! Мне так понравилось управлять машинкой, что дальнейшую часть пути я вёл в
+        прекрасном настроении.
+      </p>
       <Block>
         <ConnectTheDots
           baseWidth={670}
