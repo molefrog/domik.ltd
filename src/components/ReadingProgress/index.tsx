e674f56c35e89557d1db54fb97a9acfdf11198f9
Alexey Taktarov <molefrog@gmail.com>
2022-11-07 23:25:18 +0200

    ReadingProgress: move scroll calculation logic out of the component.

--- src/components/ReadingProgress/index.tsx
+++ src/components/ReadingProgress/index.tsx
@@ -1,52 +1,84 @@
+import styled from "@emotion/styled";
+
 import { useState, useEffect, useCallback } from "react";
-import { ProgressBar } from "./ProgressBar";
+
+import { BumperCar } from "~/components/BumperCar";
 
 interface ReadingProgressProps {
-  /*
-   * The refs of elements that define start and end of the scroll
-   */
-  startRef: React.RefObject<HTMLDivElement>;
-  endRef: React.RefObject<HTMLDivElement>;
-
+  progress: number;
   max?: number;
 }
 
-export const ReadingProgress = ({ startRef, endRef, max }: ReadingProgressProps) => {
-  const [progress, setProgress] = useState(0.0);
+export const ReadingProgress = ({ progress, max = 1.0 }: ReadingProgressProps) => {
+  const scaled = max * Math.min(1.0, Math.max(0, progress));
 
-  const onScroll = useCallback((ev?: Event) => {
-    if (endRef.current && !startRef.current) startRef = endRef;
+  const hidden = progress <= 0 || progress >= 1.0;
+  const width = 380;
 
-    if (startRef.current && endRef.current) {
-      const { top: startY } = startRef.current.getBoundingClientRect();
-      const { bottom } = endRef.current.getBoundingClientRect();
-
-      const endY = bottom - window.innerHeight;
-
-      if (startY >= 0) {
-        setProgress(0.0);
-      } else if (endY <= 0) {
-        setProgress(1.0);
-      } else {
-        const x = startY / (startY - endY);
-        setProgress(Math.round(x * 1000.0) / 1000.0); // round up 1 digit after the fpoint
-      }
-    }
-  }, []);
-
-  useEffect(() => {
-    document.addEventListener("scroll", onScroll);
-    window.addEventListener("resize", onScroll);
-
-    return () => {
-      document.removeEventListener("scroll", onScroll);
-      window.removeEventListener("resize", onScroll);
-    };
-  }, []);
-
-  useEffect(() => {
-    onScroll();
-  });
-
-  return <ProgressBar progress={progress} max={max} />;
+  return (
+    <Container hidden={hidden /*scaled >= max*/}>
+      <Bar hidden={hidden} style={{ width: width }}>
+        <Filled style={{ width: `${100.0 * scaled}%` }} />
+        <Car
+          style={{
+            transform: `
+          translate(calc(-50% + ${scaled * width}px), 0) 
+          scale(-1, 1)`,
+          }}
+        >
+          <BumperCar width={70} animation={false}></BumperCar>
+        </Car>
+      </Bar>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  position: fixed;
+  bottom: -24px;
+  left: 0;
+  right: 0;
+  height: 96px;
+  background: linear-gradient(360deg, var(--color-bg), transparent);
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  padding-bottom: 44px;
+  pointer-events: none;
+
+  ${(props) => props.hidden && `opacity: 0;`}
+  will-change: opacity;
+  transition: opacity 0.3s ease;
+  z-index: 300;
+`;
+
+const Car = styled.div`
+  position: absolute;
+  will-change: transform;
+  left: 0;
+  top: -21px;
+`;
+
+const Filled = styled.div`
+  width: 0px;
+  height: 8px;
+  background: var(--color-selected);
+  border-radius: 10px;
+`;
+
+const Bar = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+
+  border-radius: 12px;
+  background: #c1c1c1;
+  border: 2px solid white;
+
+  box-shadow: 0 0 12px 0 rgba(179, 179, 179, 0.6), 0 1px 2px 0 rgba(0, 0, 0, 0.15);
+
+  transition: all 0.25s ease;
+  will-change: transform;
+  ${(props) => props.hidden && `transform: translateY(100px);`}
+`;
