bf3bf8871454b00a65e627f57df011d842a52ad4
Alexey Taktarov <molefrog@gmail.com>
2022-11-21 14:55:17 +0100

    Buttons with real icons.

--- src/attractions/HouseBuilder/Controls.tsx
+++ src/attractions/HouseBuilder/Controls.tsx
@@ -1,4 +1,21 @@
 import styled from "@emotion/styled";
+import { css } from "@emotion/react";
+
+import iconPlay from "./images/icon-play.svg";
+import iconStop from "./images/icon-stop.svg";
+import iconCamera from "./images/icon-camera.svg";
+import iconShuffle from "./images/icon-shuffle.svg";
+import iconPlus from "./images/icon-plus.svg";
+import iconMinus from "./images/icon-minus.svg";
+
+const ICONS = {
+  play: iconPlay,
+  stop: iconStop,
+  camera: iconCamera,
+  shuffle: iconShuffle,
+  plus: iconPlus,
+  minus: iconMinus,
+};
 
 /**
  * Styles
@@ -14,7 +31,14 @@ export const Controls = styled.div`
   grid-auto-flow: column;
 `;
 
-export const Button = styled.button<{ size?: number; aspect?: string }>`
+interface ButtonProps {
+  size?: number;
+  aspect?: string;
+  icon?: keyof typeof ICONS;
+  iconSize?: string;
+}
+
+export const Button = styled.button<ButtonProps>`
   --button-size: ${({ size }) => size || 64}px;
 
   width: var(--button-size);
@@ -28,6 +52,10 @@ export const Button = styled.button<{ size?: number; aspect?: string }>`
   box-shadow: 0px 1px 0px 1.5px #ddd, 0px 2px 0px 2px #bbb, 0px -1px 0px 1px #ddd;
   font-size: calc(var(--button-size) / 2);
 
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
   &:focus-visible {
     outline: var(--color-selected-vivid) dashed 4px;
     outline-offset: 6px;
@@ -40,4 +68,16 @@ export const Button = styled.button<{ size?: number; aspect?: string }>`
   &:active {
     transform: translateY(1px);
   }
+
+  ${({ icon, iconSize = "60%" }) =>
+    icon &&
+    css`
+      &:before {
+        content: "";
+        display: inline-block;
+        width: ${iconSize};
+        height: ${iconSize};
+        background: url("${ICONS[icon!]}") center / contain no-repeat;
+      }
+    `}
 `;
