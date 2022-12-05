1dda9a77e01d3aaafc204273ef4c2cff32adf564
Alexey Taktarov <molefrog@gmail.com>
2022-12-06 00:26:06 +0100

    Compass can accept angle in radians.

--- src/components/Compass/index.tsx
+++ src/components/Compass/index.tsx
@@ -4,28 +4,35 @@ import { useSpring, animated } from "@react-spring/web";
 import body from "~/assets/compass/body.png";
 import arrow from "~/assets/compass/arrow.png";
 
-interface Props {
-  size?: number;
-  angle?: number;
-}
+type CompassProps = { size?: number } & (
+  | { angle: number; angleRad?: never } // either `angle` or `angleRad` must be present
+  | { angle?: never; angleRad: number }
+);
 
-export const Compass = (props: Props) => {
+// [0, 2π] => [0, 360]
+const radToDeg = (rad: number) => (360.0 * rad) / (2 * Math.PI);
+
+export const Compass = ({ size = 50, angle, angleRad = 0 }: CompassProps) => {
+  if (angle === undefined) {
+    angle = radToDeg(angleRad);
+  }
+
   const { alpha } = useSpring({
-    from: { alpha: 34 },
-    to: { alpha: props.angle || 0 },
+    from: { alpha: 0 },
+    to: { alpha: angle },
     config: { mass: 1, tension: 200, friction: 8 },
   });
 
   return (
-    <Body size={props.size}>
-      <Arrow style={{ rotate: alpha.to((a) => `${90.0 - a}deg`) }} />
+    <Body size={size}>
+      <Arrow style={{ rotate: alpha.to((a) => `${-a}deg`) }} />
     </Body>
   );
 };
 
-const Body = styled.div<{ size?: number }>`
-  width: ${(props) => props.size || "50"}px;
-  height: ${(props) => props.size || "50"}px;
+const Body = styled.div<{ size: number }>`
+  width: ${(props) => props.size}px;
+  height: ${(props) => props.size}px;
   background: url(${body}) no-repeat center;
   background-size: cover;
   position: relative;
