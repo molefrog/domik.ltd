9f28acbdea3dd78e44b3290d68bb1f07d0840a89
Alexey Taktarov <molefrog@gmail.com>
2022-12-07 20:59:16 +0100

    Compass: `directed` & experimental `accuracy` prop.

--- src/components/Compass/index.tsx
+++ src/components/Compass/index.tsx
@@ -3,8 +3,13 @@ import { useSpring, animated } from "@react-spring/web";
 
 import body from "~/assets/compass/body.png";
 import arrow from "~/assets/compass/arrow.png";
+import arrowDirected from "~/assets/compass/arrow-directed.png";
 
-type CompassProps = { size?: number } & (
+type CompassProps = {
+  size?: number;
+  accuracy?: number; // 0 to 1, how accurate the compass is
+  directed?: boolean;
+} & (
   | { angle: number; angleRad?: never } // either `angle` or `angleRad` must be present
   | { angle?: never; angleRad: number }
 );
@@ -12,21 +17,37 @@ type CompassProps = { size?: number } & (
 // [0, 2π] => [0, 360]
 const radToDeg = (rad: number) => (360.0 * rad) / (2 * Math.PI);
 
-export const Compass = ({ size = 50, angle, angleRad = 0 }: CompassProps) => {
+// linear interpolation
+const lerp = (t: number, a: number, b: number) => a + (b - a) * t;
+
+export const Compass = ({
+  size = 50,
+  angle,
+  accuracy = 1.0,
+  angleRad = 0,
+  directed = false,
+}: CompassProps) => {
   if (angle === undefined) {
     angle = radToDeg(angleRad);
   }
 
+  const threshold = 0.75;
+  if (accuracy < threshold) {
+    const error = accuracy / threshold;
+    angle -= (angle % Math.floor(lerp(error, 30, 15))) + lerp(error, 45, 0);
+  }
+
+  const t = 0.0;
   const { alpha } = useSpring({
     from: { alpha: 0 },
     to: { alpha: angle },
-    config: { mass: 1, tension: 200, friction: 8 },
+    config: { mass: 1, tension: lerp(accuracy, 40, 200), friction: lerp(accuracy, 0.5, 20) },
   });
 
   return (
     <Body size={size}>
       {/* zero angle starts at the east, unit circle goes counter-clockwise */}
-      <Arrow style={{ rotate: alpha.to((a) => `${-a}deg`) }} />
+      <Arrow directed={directed} style={{ rotate: alpha.to((a) => `${-a}deg`) }} />
     </Body>
   );
 };
@@ -40,13 +61,13 @@ const Body = styled.div<{ size: number }>`
   z-index: 100;
 `;
 
-const Arrow = styled(animated.div)`
+const Arrow = styled(animated.div)<{ directed: boolean }>`
   will-change: transform;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
   position: absolute;
-  background: url(${arrow}) no-repeat center;
+  background: url(${({ directed }) => (directed ? arrowDirected : arrow)}) no-repeat center;
   background-size: cover;
 `;
