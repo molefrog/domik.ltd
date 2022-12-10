ad6272143bf1919d6ef25ccd65fc05399de3509f
Alexey Taktarov <molefrog@gmail.com>
2022-12-10 14:24:10 +0100

    Allow https in development by enabling a self-signed cert. https://github.com/vitejs/vite/issues/9311

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
 import mdx from "@mdx-js/rollup";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mdx({ providerImportSource: "@mdx-js/react" })],
+  plugins: [react(), mdx({ providerImportSource: "@mdx-js/react" }), basicSsl()],
 
   resolve: {
     alias: {
