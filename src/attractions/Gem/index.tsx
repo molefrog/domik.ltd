efbbc491bacb5a9c052cce0e62f533dc94432850
Alexey Taktarov <molefrog@gmail.com>
2023-05-28 16:25:34 +0200

    3d rotation effect.

--- src/attractions/Gem/index.tsx
+++ src/attractions/Gem/index.tsx
@@ -11,19 +11,98 @@ interface Props {
   oct: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7;
 }
 
+const useNegativePhotoEffect = (active: boolean) => {
+  useEffect(() => {
+    const html = document.documentElement;
+
+    html.style.setProperty("transition", "filter 0.25s ease-in-out");
+    if (active) html.style.setProperty("filter", "invert(1)");
+
+    return () => {
+      html.style.removeProperty("filter");
+    };
+  }, [active]);
+};
+
+export const Gem = (props: Props) => {
+  const wrapperRef = useRef<HTMLElement>(null);
+  const stoneRef = useRef<HTMLElement>(null);
+
+  const isHovering = useHover(wrapperRef);
+  const mousePosition = useMouse(wrapperRef);
+
+  useNegativePhotoEffect(isHovering);
+
+  // save fresh mouse position
+  const mp = useRef<MousePosition>(mousePosition);
+  mp.current = mousePosition;
+
+  const anchorCoords = useRef<[number, number]>([0, 0]);
+
+  useEffect(() => {
+    anchorCoords.current = [mousePosition.clientX!, mousePosition.clientY!];
+  }, [mousePosition.isOver]);
+
+  useRAF(() => {
+    const el = stoneRef.current;
+    const shadowEl = wrapperRef.current;
+
+    if (!el || !shadowEl) return;
+
+    const { clientX, clientY } = mp.current;
+
+    let dx = clientX ? clientX - anchorCoords.current[0] : 0;
+    let dy = clientY ? clientY - anchorCoords.current[1] : 0;
+
+    const ll = Math.sqrt(dx * dx + dy * dy);
+    dx /= ll;
+    dy /= ll;
+
+    const l = Math.sqrt(dx * dx + dy * dy + 1);
+
+    const a = l ? dx / l : 0;
+    const b = l ? dy / l : 0;
+    const c = l ? 1 / l : 0;
+
+    const alpha = 0.4 * Math.atan2(a, c);
+    const betta = -0.4 * Math.atan2(b, c);
+
+    el.style.transform = `perspective(60px) rotate3d(0, 1, 0, ${alpha}rad) rotate3d(1, 0, 0, ${betta}rad)`;
+    shadowEl.style.setProperty("--shadow-offset", `${-a}`);
+  }, isHovering);
+
+  useEffect(() => {
+    if (!isHovering) stoneRef.current?.style.removeProperty("transform");
+  }, [isHovering]);
+
+  return (
+    <Wrapper ref={wrapperRef} hovered={isHovering}>
+      <Gemstone hovered={isHovering}>
+        <Stone
+          ref={stoneRef}
+          style={{
+            backgroundImage: `url(${symbolForOct(props.oct)})`,
+          }}
+        ></Stone>
+      </Gemstone>
+    </Wrapper>
+  );
+};
+
 const Wrapper = styled.span<{ hovered: boolean }>`
+  --shadow-offset: 1;
+
   display: inline-block;
   width: 14px;
   vertical-align: baseline;
+  transition: box-shadow 0.2s ease;
   position: relative;
-  perspective: 100px;
-  transition: box-shadow 0.4s ease 0.2s;
 
   ${({ hovered }) =>
     hovered &&
     `
     filter: invert(1);
-    box-shadow: 0px -12px 64px 32px rgb(255 255 255 / 0.4);
+    box-shadow: calc(var(--shadow-offset) * 24px) -24px 96px 64px rgb(255 255 255 / 0.35);
   `}
 `;
 
@@ -60,64 +139,3 @@ const Stone = styled.span`
 
   transition: transform 0.1s linear;
 `;
-
-/*
-
- */
-export const Gem = (props: Props) => {
-  const wrapperRef = useRef<HTMLElement>(null);
-  const stoneRef = useRef<HTMLElement>(null);
-
-  const isHovering = useHover(wrapperRef);
-  const mousePosition = useMouse(wrapperRef);
-
-  // save fresh mouse position
-  const mp = useRef<MousePosition>(mousePosition);
-  mp.current = mousePosition;
-
-  const anchorCoords = useRef<[number, number]>([0, 0]);
-
-  useEffect(() => {
-    const html = document.documentElement;
-
-    html.style.setProperty("transition", "filter 0.25s ease-in-out");
-    if (isHovering) html.style.setProperty("filter", "invert(1)");
-
-    return () => {
-      html.style.removeProperty("filter");
-    };
-  }, [isHovering]);
-
-  useEffect(() => {
-    anchorCoords.current = [mousePosition.clientX!, mousePosition.clientY!];
-  }, [mousePosition.isOver]);
-
-  useRAF(() => {
-    const el = stoneRef.current;
-    if (!el) return;
-
-    const { clientX, clientY } = mp.current;
-
-    const dx = clientX ? clientX - anchorCoords.current[0] : 0;
-    const dy = clientY ? clientY - anchorCoords.current[1] : 0;
-
-    el.style.transform = `rotate3d(1, 0, 0, ${0.5 * dx}deg) rotate3d(0, 0, 1, ${0.5 * dy}deg)`;
-  }, isHovering);
-
-  useEffect(() => {
-    if (!isHovering) stoneRef.current?.style.removeProperty("transform");
-  }, [isHovering]);
-
-  return (
-    <Wrapper ref={wrapperRef} hovered={isHovering}>
-      <Gemstone hovered={isHovering}>
-        <Stone
-          ref={stoneRef}
-          style={{
-            backgroundImage: `url(${symbolForOct(props.oct)})`,
-          }}
-        ></Stone>
-      </Gemstone>
-    </Wrapper>
-  );
-};
