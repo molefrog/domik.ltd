df40f12ae6045cbd1bd95474362710cf0d8e9443
Alexey Taktarov <molefrog@gmail.com>
2023-06-23 20:07:22 +0200

    Footer.

--- src/components/IndexPage/index.tsx
+++ src/components/IndexPage/index.tsx
@@ -1,17 +1,71 @@
 import styled from "@emotion/styled";
 import { MainMenu } from "./MainMenu";
 import { Billboard as BillboardImg } from "./Billboard";
+import { Link } from "wouter";
+import { useLocale } from "~/i18n/locale";
 
 export const IndexPage = () => {
+  const locale = useLocale();
+
   return (
-    <Menu>
-      <Billboard>
-        <BillboardImg />
-        <MainMenu />
-      </Billboard>
-    </Menu>
+    <>
+      <Menu>
+        <Billboard>
+          <BillboardImg />
+          <MainMenu />
+        </Billboard>
+      </Menu>
+
+      <Footer>
+        Unlock your fantasy home with 'Domik Limited'! ·{" "}
+        <LangLink active={locale === "en"} to="~/en">
+          English
+        </LangLink>{" "}
+        ·{" "}
+        <LangLink active={locale === "ru"} to="~/ru">
+          На русском
+        </LangLink>
+        <Sub>
+          (as long as your dreams don't exceed our budget!)
+          {" · "}
+          <a href="https://github.com/molefrog/domik.ltd" rel="noopener noreferrer" target="_blank">
+            Source on GitHub
+          </a>
+        </Sub>
+      </Footer>
+    </>
   );
 };
+
+const LangLink = styled(Link, {
+  shouldForwardProp: (prop) => !["active"].includes(prop),
+})<{ active?: boolean }>`
+  ${({ active }) => active && `font-weight: 600;`}
+`;
+
+const Sub = styled.div`
+  margin-top: 8px;
+  font-size: 18px;
+  opacity: 0.75;
+`;
+
+const Footer = styled.div`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  padding: 16px;
+
+  font-size: 20px;
+  color: var(--color-text-gray);
+  text-align: center;
+  text-shadow: 2px 0px 0px var(--color-bg), 0px 2px 0px var(--color-bg);
+
+  a {
+    color: inherit;
+    text-decoration: underline;
+  }
+`;
 
 const Menu = styled.div`
   display: flex;
@@ -35,6 +89,10 @@ const Billboard = styled.div`
   position: relative;
   width: 740px;
   flex-shrink: 0;
+
+  > svg {
+    width: 100%;
+  }
 
   @media (max-width: 380px) {
     margin-left: -24px;
