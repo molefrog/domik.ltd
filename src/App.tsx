b517392100aebddff147441b2af5484565fb20fc
Alexey Taktarov <molefrog@gmail.com>
2023-11-03 13:07:41 +0100

    Allow to switch between location hooks.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
-import { Route, Switch as Switch, Redirect } from "wouter";
-import { lazy } from "react";
+import { lazy, useEffect, useLayoutEffect } from "react";
 import styled from "@emotion/styled";
+
+import { Route, Router, Switch as Switch, useLocation, Redirect, useSearch } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
+import { useBrowserLocation } from "wouter/use-browser-location";
 
 import { CipherPage } from "~/components/CipherPage";
 import { StoryPage } from "~/components/StoryPage";
@@ -12,27 +15,42 @@ import { RoutesWithLocale } from "./i18n/locale";
 
 const PlaygroundPage = lazy(() => import("~/components/PlaygroundPage"));
 
+const RedirectToIndex = () => {
+  const [location] = useLocation();
+
+  // For some reason route inside the Switch receives updates before <RoutesWithLocale />
+  // so it doesn't get the fresh base location. This is a workaround.
+  if (location.startsWith("~/")) return null;
+
+  return <Redirect to="/" />;
+};
+
 function App() {
   usePreloadedResources();
 
+  const search = useSearch();
+  const locationHook = search === "hash-router" ? useHashLocation : useBrowserLocation;
+
   return (
-    <RoutesWithLocale>
-      <Body>
-        <Switch>
-          <Route path="/" component={IndexPage} />
-          <Route path="/x/*?" component={CipherPage} />
-          <Route path="/story/*?" component={StoryPage} />
+    <Router hook={locationHook}>
+      <RoutesWithLocale>
+        <Body>
+          <Switch>
+            <Route path="/" component={IndexPage} />
+            <Route path="/x/*?" component={CipherPage} />
+            <Route path="/story/*?" component={StoryPage} />
 
-          {import.meta.env.DEV && <Route path="/playground" component={PlaygroundPage} />}
+            {import.meta.env.DEV && <Route path="/playground" component={PlaygroundPage} />}
 
-          <Route>
-            <Redirect to="/" />
-          </Route>
-        </Switch>
+            <Route>
+              <RedirectToIndex />
+            </Route>
+          </Switch>
 
-        <TVPlayer />
-      </Body>
-    </RoutesWithLocale>
+          <TVPlayer />
+        </Body>
+      </RoutesWithLocale>
+    </Router>
   );
 }
 
